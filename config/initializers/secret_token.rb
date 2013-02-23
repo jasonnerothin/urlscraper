f4c6b53b86b2f9c3da972a85007874e0b3013873
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-02-22 20:14:40 -0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlscraper::Application.config.secret_token = '29efd367e243aa540188cf35875e7725f8a0f951261f9c8f33e9564a0041eebee76f08b1ebd13ecd17c14a8dfe59d0acf67fb534d72bff9b4962757369c43518'
