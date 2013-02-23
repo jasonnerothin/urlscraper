051f7a484a944afb5f1ab3b8bd0223066469ba2a
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-02-23 14:32:21 -0600

    Detail route worked out, especially error conditions.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,14 +1,34 @@
 class PageController < ApplicationController
   #protect_from_forgery
+
   def index
     @pages = Page.all
     render 'page/index'
   end
+
   def form
     render 'page/form'
   end
+
+  # show details about a single page, or
+  # redirect to an error page if necessary
   def detail
-    @page = Page.find_by()
-    render 'page/detail'
+    rendered = false
+    @id = params[:id]
+    if @id.nil?
+      render 'page/noresult'
+      rendered = true
+    end
+    begin
+      @page = Page.find id unless rendered
+    rescue
+      render 'page/noresult' unless rendered
+      rendered = true
+    end
+    respond_to do |format|
+      format.html render 'page/detail'
+      format.json :json => @post
+    end unless rendered
   end
+
 end
--- test/functional/page_controller_test.rb
+++ test/functional/page_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
 class PageControllerTest < ActionController::TestCase
+
   test "should get index" do
     get :index
     assert_response :success
   end
+
+  test "should get error page when no id supplied" do
+
+    actual = get :detail
+    assert_response :success
+    @page = actual.body
+    assert @page.index( "id required") > -1
+
+  end
+
+  test "should get an error page when no record found" do
+
+    actual = get :detail, :id => 2
+    assert_response :success
+    @page = actual.body
+    assert @page.index("does not correspond to a Page") > -1
+
+  end
+
 
 end
